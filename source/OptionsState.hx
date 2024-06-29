eea23026037cb977aef57f0ef3df7f985d58b669
brusakas <121342424+brusakas@users.noreply.github.com>
2024-06-29 22:52:33 +0100

    update errorsing

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -31,7 +31,7 @@ using StringTools;
 // TO DO: Redo the menu creation system for not being as dumb
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Notes', 'Keyboard Controls', 'Mobile Controls', 'Preferences'];
+	var options:Array<String> = ['Notes', 'Keyboard Controls', 'Mobile is gay Controls', 'Preferences'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;		
@@ -916,7 +916,7 @@ class PreferencesSubstate extends MusicBeatSubstate
 						ClientPrefs.noteSplashes = !ClientPrefs.noteSplashes;
 						
 					case 'Hitsounds':
-					    ClientLrefs.hitsounds = !ClientPrefs.hitsounds
+					        ClientPrefs.hitsounds = !ClientPrefs.hitsounds
 
 					case 'Flashing Lights':
 						ClientPrefs.flashing = !ClientPrefs.flashing;
